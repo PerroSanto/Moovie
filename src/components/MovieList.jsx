de9d8df5fe725a280e84320f9d2294897de5834c
Martin Acuña <msguser@gmail.com>
2021-07-14 20:08:47 -0300

    add year

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -12,19 +12,22 @@ return (
                 { movie.poster_path === null ? <div className="h-96 w-64 max-w-screen-xl shadow-2xl rounded-xl bg-gray-800 relative text-white"> Not available</div> :
                 <div>
                     <img className="absolute inset-0 h-half w-half rounded-xl shadow-xl" src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} alt="image"/> 
-                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 text-transparent hover:bg-opacity-75 hover:text-white">
-                        <div className="m-2">
-                            <h1 className="items-start justify-center">Popularidad: {Math.ceil(movie.popularity)} / 100</h1>
+                    <div className="absolute inset-0 bg-gray-900 bg-opacity-0 text-transparent duration-300 hover:bg-opacity-75 hover:text-white">
+                        <div className="m-2 ">
+                            <h1 className="justify-center tracking-wider text-sm">Popularidad: {Math.ceil(movie.popularity)} / 100</h1>
                         </div>
-                        <div className=" p-2 mx-2 my-16 h-72 overflow-y-auto scrollbar">
+                        <div className=" p-2 mx-2 my-8 h-72 overflow-y-auto scrollbar">
                         <h1 className="tracking-wider items-center text-sm text-justify">{movie.overview}</h1>
                         </div>
                     </div>
                 </div>
                 }
                 </div>
-                <div className="text-left text-xl text-white p-3">
+                <div className="text-left text-md text-white p-2">
                     <h1>{movie.original_title}</h1>
+                    { !movie.release_date ? <div></div> :
+                        <h1 className="text-yellow-300 text-sm text-opacity-70">{movie.release_date.split("-" , 1)}</h1>
+                    }
                 </div>
             </div>
         </div>
