693fa36087e09a71bbed49c7a875980358c5e4f4
Martin Acuña <msguser@gmail.com>
2021-07-14 16:19:36 -0300

    responsive logo

--- src/components/LHeader.jsx
+++ src/components/LHeader.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LHeader = (props) => {
     return (
         <div className="bg-black m-0 p-2">
-          <div className="bg-yellow-300 m-0 p-1 rounded-lg container mx-1 w-32">
+          <div className="bg-yellow-300 m-0 p-1 rounded-lg container mx-1 w-24">
             <h1 className= "font-mono text-2xl text-black font-extrabold tracking-tighter antialiased">Moovie</h1>
           </div>
         </div>
