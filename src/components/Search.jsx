8f709a2422dc2d8288cae2c3c7a11c9864a4fef8
Martin Acuña <msguser@gmail.com>
2021-07-14 23:58:23 -0300

    find button

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import find from '../find.svg';
 
 const SearchBox = (props) => {
     return (
-        <div className="bg-black m-0 p-2"> 
-            <input className= "border rounded w-full py-2 px-3 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:border-transparent text-gray-700 leading-tight" 
-            value={props.value}
-            onChange={(event) => props.setSearchValue(event.target.value)}
-            placeholder="Search Moovie"/>
+        <div className="bg-black m-0 p-2 grid-cols-2 flex"> 
+            <input id="SearchBox" className= "border rounded-l-lg w-full py-2 px-3 focus:outline-none focus:border-4 focus:border-yellow-300  text-gray-700 leading-tight" 
+                value={props.value}
+                placeholder="Search Moovie" />
+            <div className="bg-yellow-300 w-8 rounded-r-lg cursor-pointer hover:bg-yellow-400">
+                <img src={find} onClick={(event) => props.setSearchValue(document.getElementById("SearchBox").value)} alt="image"/>
+            </div>
         </div>
     )
 }; export default SearchBox;
