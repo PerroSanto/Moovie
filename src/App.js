a4bf1f028150ed5383ae0fb52ff01e637c0fcfdd
Martin Acuña <msguser@gmail.com>
2021-07-17 13:42:21 -0300

    favoritos

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import MovieList from './components/MovieList';
 import LHeader from './components/LHeader';
 import SearchBox from './components/Search'
 import RHeader from './components/RHeader';
+import AddFavorites from './components/AddFavorites';
+import DelFavorites from './components/DelFavorites';
+import FavoriteList from './components/FavoriteList';
 
 function App() {
 
   const [ movies, setMovies ] = useState([]);
+
+  //Agrego el hook para el manejo de los favoritos
+  const [ favorites, setFavorites ] = useState([]);
+
   const [ searchValue, setSearchValue ] = useState('');
 
   const getMovieRequest = async (searchValue) => {
@@ -14,27 +21,89 @@ function App() {
     const api = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=es-AR&query=${searchValue}&page=1&include_adult=false`
     const response = await fetch(api);
     const responseJson = await response.json();
+    console.log(responseJson)
     // Esto lo pongo para que cuando inicia la pagina no traiga un array vacio y de error el map.
     if (responseJson.results) {
       setMovies(responseJson.results)
-    }
+    } 
   };
+  
   //Al tipear se le pasa el searchValue al hook, el hook pasa el valor a la funcion getMovieRequest
   useEffect(() => {
   getMovieRequest(searchValue);
   }, [searchValue]);
 
+  //Este hook permite que los favoritos se cargen desde el localStorage al iniciar la pagina
+  //lo convertimos de string a objeto nuevamente con parse
+  useEffect(() => {
+    const movieList = JSON.parse(localStorage.getItem('moovie-favorites'));
+    //Agrego este condicional para evitar un error al cargar la app cuando no existe una lista de favoritos.
+    if (movieList) {
+    setFavorites(movieList)
+    }
+  }, []);
+
+  //agregamos una pelicula al array favorites
+  //llamamos a la funcion setFavorites y le pasamos una nueva lista que incluye el array anterior
+  const AddFavoriteMovie = (movie) => {
+    //agrego un condicional para que no se dupliquen los favoritos
+    const existInFavorites = favorites.find(
+      (favorite) => favorite.id === movie.id)
+
+    if (!existInFavorites) {
+      const newFavoriteList = [...favorites, movie]
+      setFavorites(newFavoriteList)
+      saveLocalStorage(newFavoriteList)
+    } else {
+      //agregar exepcion!
+      window.alert("Esta pelicula ya esta en tus favoritos");
+    }
+  };
+
+  //Borramos una pelicula del array favorites
+  //Como vimos en clase usamos un filtro para crear un nuevo array que no contenga la pelicula que queremos sacar de la lista
+  const DelFavoriteMovie = (movie) => {
+    const newFavoriteList = favorites.filter(
+      (favorite) => favorite.id !== movie.id
+    )
+      setFavorites(newFavoriteList)
+  };
+
+  //agrego funcionalidad para local storage
+  const saveLocalStorage = (movieList) => {
+    localStorage.setItem('moovie-favorites', JSON.stringify(movieList))
+  }
+
   return (
     <div className="App text-center">
       <div className="grid grid-cols-3 gap-0">
-        <LHeader />
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+        <LHeader/>
+        <SearchBox searchValue={searchValue} 
+        setSearchValue={setSearchValue}/>
         <RHeader />
       </div>
+
+            <div >
+        <h1 className="text-3xl text-white">Favorites</h1>
+        <FavoriteList movies={favorites} 
+        favoriteClick={DelFavoriteMovie} 
+        favoriteComponent={DelFavorites} />
+      </div>
+
       <div >
-        <MovieList movies={movies} />
+      <h1 className="text-3xl text-white">Moovies</h1>
+        <MovieList movies={movies} 
+        favoriteClick={AddFavoriteMovie} 
+        favoriteComponent={AddFavorites} />
       </div>
     </div>
   );
 } export default App;
 
+//Si el buscador es null popup
+//cambiar favoritesComponent
+//cambiar AddFavorites
+//agregar exepciones con popup
+//agregar respuesta cuando no se encuentra la pelicula deseada 
+//agregar proteccion para que no se pueda seleccionar varias veces la misma pelicula
+//agrepar popup si existe pelicula en favoritos.
--- src/components/FavoriteList.jsx
+++ src/components/FavoriteList.jsx
@@ -1,38 +1,35 @@
 import React from 'react';
 
-const MoviesList = (props) => {
+const FavoriteList = (props) => {
     //pasamos el componente desde app y lo ubicamos en el div correspondiente
     const FavoriteComponent = props.favoriteComponent;
 return (
     <>
-    <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div className=" flex overflow-x-auto scrollbar">
     {props.movies.map((movie, index) => (
         <div className="p-8">
-            <div key={index} className="grid-cols-2">
-                <div className="h-96 w-64 max-w-screen-xl relative ">
+            <div key={index}>
+                <div className="h-72 w-48 max-w-screen-xl relative">
                 { movie.poster_path === null ? <div className="h-96 w-64 max-w-screen-xl shadow-2xl rounded-xl bg-gray-800 relative text-white"> Not available</div> :
                 <div>
                     <img className="absolute inset-0 h-half w-half rounded-xl shadow-xl" src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} alt="image"/> 
                     <div className="absolute inset-0 bg-gray-900 bg-opacity-0 text-transparent duration-300 hover:bg-opacity-75 hover:text-white">
-                        <div className="m-2 grid grid-cols-5">
-                            <h1 className="text-left tracking-wider text-sm col-span-4" >Popularidad: {Math.ceil(movie.popularity)} / 100</h1>
+                        <div className="m-2 ">
+                            <h1 className="justify-center tracking-wider text-sm">Popularidad: {Math.ceil(movie.popularity)} / 100</h1>
                             
                             <div onClick={() => props.favoriteClick(movie)}>
                             <FavoriteComponent />
                             </div>
 
                         </div>
-                        <div className=" p-2 mx-2 my-8 h-72 overflow-y-auto scrollbar">
-                        <h1 className="tracking-wider items-center text-sm text-justify">{movie.overview}</h1>
-                        </div>
                     </div>
                 </div>
                 }
                 </div>
-                <div className="text-left text-md text-white p-2">
+                <div className="text-left text-sm text-white p-2">
                     <h1>{movie.original_title}</h1>
                     { !movie.release_date ? <div></div> :
-                        <h1 className="text-yellow-300 text-sm text-opacity-70">{movie.release_date.split("-" , 1)}</h1>
+                        <h1 className="text-yellow-300 text-xs text-opacity-70">{movie.release_date.split("-" , 1)}</h1>
                     }
                 </div>
             </div>
@@ -41,4 +38,4 @@ return (
     </div>
     </>
 );
-}; export default MoviesList;
+}; export default FavoriteList;
